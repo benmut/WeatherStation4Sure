1e83df2bcc77863cb107a10509ad9f19d4ab38bc
Ben Mutondo <ben_mutondo@hotmail.com>
2025-02-13 22:27:54 +0200

    update route and navgraph for map and dayforecast screens navigation

--- app/src/main/java/com/mutondo/weatherstation4sure/navigation/AppNavHost.kt
+++ app/src/main/java/com/mutondo/weatherstation4sure/navigation/AppNavHost.kt
@@ -2,10 +2,13 @@ package com.mutondo.weatherstation4sure.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.mutondo.weatherstation4sure.map.MapScreen
+import com.mutondo.weatherstation4sure.weather_forecast.presentation.DaysForecastScreen
 
 @Composable
 fun AppNavHost(
@@ -13,6 +16,7 @@ fun AppNavHost(
     startDestination: String = Destination.Map.route
 ) {
     val navController = rememberNavController()
+    val navigator = NavigatorImpl(navController)
 
     NavHost(
         modifier = modifier,
@@ -20,7 +24,23 @@ fun AppNavHost(
         startDestination = startDestination
     ) {
         composable(Destination.Map.route) {
-            MapScreen()
+            MapScreen(navigator = navigator)
+        }
+        composable(
+            route = "${Destination.DaysForecast.route}/{$LATITUDE_KEY}/{$LONGITUDE_KEY}",
+            arguments = listOf(
+                navArgument(LATITUDE_KEY) {
+                    type = NavType.StringType
+                },
+                navArgument(LONGITUDE_KEY) {
+                    type = NavType.StringType
+                }
+            )
+        ) { navBackStackEntry ->
+            DaysForecastScreen(
+                navigator = navigator,
+                navBackStackEntry = navBackStackEntry
+            )
         }
     }
 }
--- app/src/main/java/com/mutondo/weatherstation4sure/navigation/Route.kt
+++ app/src/main/java/com/mutondo/weatherstation4sure/navigation/Route.kt
@@ -1,5 +1,9 @@
 package com.mutondo.weatherstation4sure.navigation
 
+const val LATITUDE_KEY = "latitude_key"
+const val LONGITUDE_KEY = "longitude_key"
+
 sealed class Destination(val route: String) {
     data object Map : Destination("map_screen")
+    data object DaysForecast : Destination("days_forecast_screen")
 }
